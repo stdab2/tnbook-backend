f8c28f62ea43a74916f6f61d991a7ce5c285b1ec
Steeve Dabire <steeve.dabire.1@ulaval.ca>
2025-08-19 01:33:38 -0400

    Initial setup

---
+++ src/main/java/com/tnbook/tnbook/TnbookApplication.java
@@ -0,0 +1,13 @@
+package com.tnbook.tnbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tnbook/tnbook/TnbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnbook.tnbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
